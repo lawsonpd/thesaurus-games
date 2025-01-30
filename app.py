6b437cac0a03f42b606d80371126d34181b270d6
Peter Lawson <lawsonpd@pm.me>
2025-01-30 02:10:44 -0600

    Fix typo.

--- app.py
+++ app.py
@@ -242,7 +242,7 @@ def process_input():
                 </div>
                 <div class="success-message">Congratulations! The word was '{{ target_word }}'</div>
                 <div class="box mt-4">
-                    <h3 class="subtitle is-6 mb-2">All synonyms for this {{ target_word }}:</h3>
+                    <h3 class="subtitle is-6 mb-2">All synonyms for {{ target_word }}:</h3>
                     <div class="tags are-medium">
                         {% for word in displayed %}
                             <span class="tag">{{ word }}</span>
